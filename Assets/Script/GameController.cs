fef83caa91dc5e6a6dd7abbfc7f60bee453011d0
Qnbie <alszenful@gmail.com>
2021-08-09 10:43:34 +0200

    GameController.cs set up state machine

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -11,16 +11,62 @@ namespace Script
 
         public BoardController boardControllerPrefab;
 
-        public event DeSelectDelegate OnDeSelectAll;
+        public event StartTurnDelegate StartTurn;
+        public event EndTurnDelegate EndTurn;
         
         private BoardController _boardController;
-        
+        private GameState _gameState;
+
         private void Start()
         {
-            BoardController boardController = Instantiate(boardControllerPrefab, transform.position, Quaternion.identity);
-            boardController.name = "BoardController";
+            _gameState = GameState.PRETurnPhase;
+        }
+
+        private void GameLoop()
+        {
+            while (_gameState != GameState.ExitGame)
+            {
+                switch (_gameState)
+                {
+                    case GameState.PRETurnPhase:
+                        _gameState = GameState.StartTurnPhase;
+                        break;
+                    case GameState.StartTurnPhase:
+                        OnStartTurn();
+                        _gameState = GameState.ActionPhase;
+                        break;
+                    case GameState.ActionPhase:
+                        _gameState = GameState.EndTurnPhase;
+                        break;
+                    case GameState.EndTurnPhase:
+                        OnEndTurn();
+                        _gameState = GameState.PRETurnPhase;
+                        break;
+                    case GameState.ExitGame:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private void OnStartTurn()
+        {
+            StartTurn?.Invoke();
+        }
+
+        private void OnEndTurn()
+        {
+            EndTurn?.Invoke();
         }
     }
 
-    public delegate void DeSelectDelegate();
+    public delegate void StartTurnDelegate();
+    public delegate void EndTurnDelegate();
+
+    public enum GameState
+    {
+        PRETurnPhase, StartTurnPhase, ActionPhase, EndTurnPhase,
+        ExitGame
+    }
 }
