82607b782016d6d70bb5f9d1259d978f0fa5c4af
Qnbie <alszenful@gmail.com>
2021-08-09 12:21:27 +0200

    CodeDuplication

--- Assets/Script/GameBoard/BoardController.cs
+++ Assets/Script/GameBoard/BoardController.cs
@@ -38,5 +38,24 @@ namespace Script.GameBoard
             }
         }
 
+        
+        public void Populate2()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                _tileMatrix.Add(new List<TileController>());
+                for (int j = 0; j < 4; j++)
+                {
+
+                    Vector3 position = new Vector3(
+                        i*tilePrefab.MyTileRender.size.x + i*offset,
+                        transform.position.y,
+                        j*tilePrefab.MyTileRender.size.z + j*offset);
+                    TileController tile = Instantiate(tilePrefab, position, Quaternion.identity);
+                    _tileMatrix[i].Add(tile);
+                    tile.name = "Tile " + i + " " + j;
+                }
+            }
+        }
     }
 }
